76e36fd8a9e6823013f60995c8b7c92ce66e98b5
25a35a0516 <25a35a0516@pragati.ac.in>
2026-03-17 19:49:55 +0530

    Time: 17 ms (20.01%), Space: 47.7 MB (62.26%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i:nums){
+            map.put(i,map.getOrDefault(i,0)+1);
+        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>(
+            (a,b) -> map.get(a) - map.get(b)
+        );
+        for(int i : map.keySet()){
+            pq.add(i);
+            if(pq.size()>k){
+                pq.poll();
+            }
+        }
+        int[] res = new int[k];
+        int i=0;
+        while(!pq.isEmpty()){
+            res[i++]=pq.poll();
+        }
+        return res;
+    }
+}
