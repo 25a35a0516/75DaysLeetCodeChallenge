5ab03601bf521c84c65af688df61883438b537b4
25a35a0516 <25a35a0516@pragati.ac.in>
2026-03-21 12:58:09 +0530

    Time: 1400 ms (6.93%), Space: 55.8 MB (28.15%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int min = nums.length/2;
+        for(int i=0; i<nums.length; i++){
+            int count = 0;
+            for(int j=0; j<nums.length; j++){
+                if(nums[i]==nums[j]) count ++;
+            }
+            if(count > min) return nums[i];
+        }
+        return 0;
+    }
+}
