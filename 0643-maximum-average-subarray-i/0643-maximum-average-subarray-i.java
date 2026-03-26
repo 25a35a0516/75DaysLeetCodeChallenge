620c93e96b35c477ff5d0a3eef7858e5865a75f0
25a35a0516 <25a35a0516@pragati.ac.in>
2026-03-26 12:49:01 +0530

    Time: 2576 ms (5%), Space: 69.9 MB (18.67%) - LeetHub

---
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        int n = nums.length;
+        double max = Double.NEGATIVE_INFINITY;
+        for(int i=0; i<=n-k; i++){
+            int sum = 0; 
+            for(int j=i; j<i+k; j++){
+                sum += nums[j];
+            }
+            double avg = (double) sum/k;
+            max = Math.max(max,avg);
+        }
+        return max;
+    }
+}
